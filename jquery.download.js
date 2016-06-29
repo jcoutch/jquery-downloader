edffe404bfdca8eb367b4c17a6def8963fcd7be6
jcoutch <jcoutch@users.noreply.github.com>
2016-06-29 14:23:00 -0400

    Initial Version

---
+++ jquery.download.js
@@ -0,0 +1,22 @@
+$.download = function(settings) {
+  var newSettings = $.extend(true, {}, settings);
+  var oldSuccess = settings.success || function(data) { return { fileName: "download", data: data }; };
+  newSettings.success = function() {
+    // { fileName: '', contentType: '', data: '' }
+    var result = settings.success.apply(this, arguments);
+
+    if (window.navigator.msSaveBlob)
+      window.navigator.msSaveBlob(result.data, result.fileName);
+    else
+    {
+      var blob = new Blob([result.data], { type: result.contentType || 'application/octet-stream' });
+      var link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
+      link.href = URL.createObjectURL(blob);
+      link.download = result.fileName;
+      link.click();
+      URL.revokeObjectURL(link.href);
+    }
+  };
+
+  $.ajax(newSettings);
+};
